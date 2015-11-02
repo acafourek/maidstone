350721aa6ba58371072efdcd0a1b8f19c1e1b6cf
Andrew Cafourek <andrew@alumnispaces.com>
2015-11-02 10:38:44 -0800

    fix content tag

--- wp-content/themes/maidstone/work-page.php
+++ wp-content/themes/maidstone/work-page.php
@@ -21,7 +21,7 @@ get_header(); ?>
 
 				<?php while ( have_posts() ) : the_post(); ?>
 
-					<?php get_template_part( 'content', 'work' ); ?>
+					<?php get_template_part( 'content'); ?>
 
 				<?php endwhile; // end of the loop. ?>
 
